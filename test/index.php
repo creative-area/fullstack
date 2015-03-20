3be5cb2dbdd89ba9d09af7eb501edf5a8a501c1c
jaubourg <j@ubourg.net>
2015-03-20 01:06:30 +0100

    Better organization of the test folder

--- test/index.php
+++ test/index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once __DIR__."/autoload.php";
+set_include_path(implode(PATH_SEPARATOR, array(get_include_path(), __DIR__ . "/..")));
+spl_autoload_register();
 
 header("Content-Type: text/plain");
 
