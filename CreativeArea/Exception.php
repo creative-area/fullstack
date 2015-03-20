c6ac8b9753412ff272c8086e50ee9a005b8916e1
jaubourg <j@ubourg.net>
2015-03-20 05:59:08 +0100

    remove unnecessary constructor

--- CreativeArea/Exception.php
+++ CreativeArea/Exception.php
@@ -1,9 +1,8 @@
 <?php namespace CreativeArea;
 
+/**
+ * Class Exception.
+ */
 class Exception extends \Exception
 {
-    public function __construct($message = "", $code = 0, $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
--- CreativeArea/FullStack/Exception.php
+++ CreativeArea/FullStack/Exception.php
@@ -1,9 +1,8 @@
 <?php namespace CreativeArea\FullStack;
 
+/**
+ * Class Exception.
+ */
 class Exception extends \CreativeArea\Exception
 {
-    public function __construct($message = "", $code = 0, $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
